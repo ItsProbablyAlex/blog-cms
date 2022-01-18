455b9c6d6f1e55fab241efaec30394cecdaa5832
Alexander Chapman <achapman135@gmail.com>
2022-01-18 16:24:02 +0000

    Added Contact and About to Schema

---
+++ src/api/contact-method/controllers/contact-method.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  contact-method controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::contact-method.contact-method');
---
+++ src/api/contact-method/services/contact-method.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-method service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::contact-method.contact-method');
