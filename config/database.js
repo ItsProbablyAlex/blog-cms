4288b9b36aa75d8ba376f5b5c5fdf40624ae99a5
Alexander Chapman <achapman135@gmail.com>
2022-01-18 12:13:12 +0000

    Use postgres

--- config/database.js
+++ config/database.js
@@ -1,36 +1,11 @@
 const path = require('path');
-const parse = require('pg-connection-string').parse;
 
-module.exports = ({env}) => {
-  if (process.env.NODE_ENV == 'production') {
-    const config = parse(process.env.DATABASE_URL); 
-    config.ssl = { rejectUnauthorized: false };
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: true
-          },
-        },
-        debug: false,
-      },
-    };
-  } else {
-    return {
-      connection: {
-        client: 'sqlite',
-        connection: {
-          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-        },
-        useNullAsDefault: true,
-        debug: true,
-      },
-    };
-  }
-};
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
